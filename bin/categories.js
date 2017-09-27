3e030b749a7b80db0cbc68332bd0bbbfaf829d56
Tim Evans <tim_evans@condenast.com>
2017-09-27 16:00:11 -0400

    lookup categories for rules via a categories file and try to add documentation pulled from ember-template-lint

---
+++ bin/categories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  'attribute-indentation': ['Clarity', 'Style'],
+  'bare-strings': ['Compatibility'],
+  'block-indentation': ['Clarity', 'Style'],
+  'duplicate-attributes': ['Bug Risk'],
+  'deprecated-each-syntax': ['Bug Risk', 'Compatibility'],
+  'deprecated-inline-view-helper': ['Bug Risk', 'Compatibility'],
+  'html-comments': ['Style'],
+  'img-alt-attributes': ['Compatibility'],
+  'inline-link-to': ['Clarity', 'Style'],
+  'inline-styles': ['Clarity', 'Style'],
+  'invalid-interactive': ['Bug Risk', 'Compatibility'],
+  'linebreak-style': ['Clarity'],
+  'link-rel-noopener': ['Security'],
+  'nested-interactive': ['Bug Risk', 'Compatibility'],
+  'no-debugger': ['Style'],
+  'no-log': ['Style'],
+  'self-closing-void-elements': ['Compatibility', 'Style'],
+  'simple-unless': ['Clarity', 'Style'],
+  'style-concatenation': ['Security'],
+  'template-length': ['Complexity', 'Style'],
+  'triple-curlies': ['Security'],
+  'unused-block-params': ['Clarity', 'Complexity']
+};
